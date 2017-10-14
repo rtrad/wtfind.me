97f0649a34375b510d5bc97b2f7fa291099fe7ba
nikhil <nikhil4596@gmail.com>
2017-10-14 07:21:09 -0400

    Added user page and more regiter fields

--- static/index.js
+++ static/index.js
@@ -7,10 +7,16 @@ var txtsignInPassword = document.getElementById('password');
 var txtregisterPassword = document.getElementById('password2');
 var txtregisterPassword2 = document.getElementById('password3');
 var currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+var email = document.getElementById('email');
+var phone = document.getElementById('phone');
+var name = document.getElementById('fullName');
 
-function User(username, password) {
+function User(username, password, email, phone, name) {
     this.username = username;
     this.password = password;
+    this.email = email;
+    this.phone = phone;
+    this.name = name;
 }
 
 function verifyPass() {
@@ -31,7 +37,10 @@ $('#register').click(function(){
     if (verifyPass()) {
         var username = txtregisterUsername.value;
         var pass = txtregisterPassword.value;
-        currentUser = new User(username, pass);
+        var email = email.value;
+        var phone = phone.value;
+        var name = name.value;
+        currentUser = new User(username, pass, email, phone, name);
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "/register", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
@@ -50,7 +59,7 @@ $('#register').click(function(){
 $('#login').click(function(){
     var username = txtsignInUsername.value;
     var pass = txtsignInPassword.value;
-    currentUser = new User(username, pass);
+    currentUser = new User(username, pass, null, null, null);
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/login", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
