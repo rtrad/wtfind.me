fdec6bd7b31741db36bcfb8679999708b152098b
Ryan Trad <trad.ryan.alan@gmail.com>
2017-10-13 22:07:28 -0400

    created flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+import boto3
+
+boto_session = boto3.Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+
+dynamodb = boto_session.resource('dynamodb', region_name='us-west-2')
+users = dynamodb.Table('wtfindme-users')
+
+app = Flask(__name__)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
+
